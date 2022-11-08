c2e5eae5eb5625d1b5ba9f52b3c6126e35dbd2ff
Ramiro Iglesias <riglesias@birt.eus>
2022-11-08 13:47:34 +0100

    Unique identifier for each job execution

--- src/main/java/birt/eus/gyropendatadownload/JobScheduler.java
+++ src/main/java/birt/eus/gyropendatadownload/JobScheduler.java
@@ -1,6 +1,7 @@
 package birt.eus.gyropendatadownload;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -8,6 +9,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
 @Component
 @EnableScheduling
 @RequiredArgsConstructor
@@ -15,12 +20,18 @@ public class JobScheduler {
 
   private final JobLauncher jobLauncher;
   private final Job job;
+  private AtomicInteger jobCounter = new AtomicInteger(0);
 
   /**
    * Launch the batch job every day at 00:00
    */
   @Scheduled(cron = "0 0 0 ? * *")
   public void launchJob() throws Exception {
-    jobLauncher.run(job, new JobParametersBuilder().toJobParameters());
+    jobLauncher.run(job,
+      new JobParametersBuilder()
+        .addDate("executionDate", new Date())
+        .toJobParameters());
+    jobCounter.incrementAndGet();
+    log.info("Job number {} completed", jobCounter.get());
   }
 }
